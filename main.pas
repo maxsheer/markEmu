a177d459e5d93c27b0ab17819c45eb59f05d00be
wclayton <wclayton@student.21-school.ru>
2019-12-16 05:45:11 +0300

    halfdone

--- main.pas
+++ main.pas
@@ -25,20 +25,59 @@ Pstack=^callstack;
 callstack=record 
 	search: string;
 	replace: string;
-	terminate: (0, 1);
+	terminate: integer;
 	next: Pstack;
 end;
 
 var 
 finp, fout, ftra: text;
-mainstr, tmp: string;
-i,j,k: int;
+mainstr, tmp, tmpser, tmprep: string;
+i,j,k. tmpter: int;
+bus: Pstack;
+
 
 
 
 {functions}
 {procedures}
 
+function lstinit(s, r: string; t: integer): Pstack;
+var
+out: Pstack;
+begin
+  new(out);
+  out^.search := s;
+  out^.replace := r;
+  out^.terminate := t;
+  out^.next := nil;
+  lstinit := out;
+end;
+
+procedure list_pushback(var lst: Pstack; s, r: string; t: integer);
+var
+add, tmp: Pstack;
+begin
+	new(add);
+	add^.search := s;
+	add^.replace := r;
+	add^.terminate := t;
+	add^.next := nil;
+	tmp := lst;
+	while (tmp^.next <> nil) do
+		tmp := tmp^.next;
+	tmp^.next := add;
+end;
+
+procedure freelst(var lst: P);
+begin
+    if (lst <> nil) then
+    begin
+        freelst(lst^.next);
+        lst^.st := '';
+        lst^.next := nil;
+        dispose(lst);
+    end;
+end;
 
 function replace(var st: string; needle: string; rep: string)
 var
@@ -67,16 +106,49 @@ begin
 	reset(finp);
 	reset(fout);
 	reset(ftra);
+	bus := nil;
 
 	readln(tmp, finp);
-	mainstr = tmp;
+	mainstr := tmp;
 	readln(tmp);
-	while ((tmp[i] >= 'a') and (tmp[i] <= 'z')) 
-		or ((tmp[i] >= 'A') and (tmp[i] <= 'Z')) then
+	while not eof(finp) do
 	begin
-		mainstr[i] = tmp[i];
-		i := i + 1;
+		readln(tmp);
+		i := 1;
+
+		tmpser := '';
+		while (tmp[i] <> '-') and (tmp[i] <> '|')
+		and (i <= length(tmp)) do
+		begin
+			tmpser[i] := tmp[i];
+			i := i + 1;
+		end;
+		if (tmp[i] = '|') then
+		begin
+			tmpser := 1;
+			i := i + 4;
+		end
+		else
+		begin
+			tmpser := 0;
+			i := i + 3;
+		end;
+		tmprep := '';
+		j := 1;
+		while (i <= length(tmp)) do
+		begin
+			tmprep[j] := tmp[i];
+			i := i + 1;
+			j := j + 1;
+		end;
+		if (bus = nil) then
+			bus := initlst(tmpser, tmprep, tmpter)
+		else
+			list_pushback(bus, tmpser, tmprep, tmpter);
 	end;
 
+	{now let the main process begin}
+
+	
 
 end.
